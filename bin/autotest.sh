15f28c9d28d105dfc093a43c4c525e4dcdddd5a5
niri81 <38499069+niri81@users.noreply.github.com>
2026-01-29 15:10:19 +0100

    ci: use podman or docker for tests

--- bin/autotest.sh
+++ bin/autotest.sh
@@ -1,6 +1,17 @@
+#! /bin/bash
+
+if command -v podman &>/dev/null; then
+  runner=podman
+elif command -v docker &>/dev/null; then
+  runner=docker
+else
+  echo "Either docker or podman must be installed for tests to run"
+  exit 1
+fi
+
 watchexec \
   --watch . \
   --clear \
   --ignore 'tests/**/diff/**' \
   --ignore 'tests/**/out/**' \
-  "typst-test r"
+  "$runner run -v $(pwd):/data ghcr.io/typst-community/tytanic r --root /data"
